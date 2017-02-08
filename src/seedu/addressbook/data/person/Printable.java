0363de72fa19f28c0213d2e8969fcf51c948edb2
Crsytral <shouyu2@hotmail.com>
2017-02-08 23:12:15 +0800

    Add a Printable interface
    Made Name, Phone, Email, and Address classes implements Printable
    interface.

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,9 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface for Phone, Name, Email and Address class in the addressbook 
+ * so that each class produces a printable string representation of the object.
+ */
+public interface Printable {
+	public String getPrintableString();
+}
